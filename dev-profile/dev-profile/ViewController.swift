92f559b9820bf8bc70eab817d3b48cc0c791990c
Xantes Fabela <xantesfabela@172-242-4-124.cust.exede.net>
2017-10-12 17:30:55 -0500

    dev layout and constraint

--- dev-profile/dev-profile/ViewController.swift
+++ dev-profile/dev-profile/ViewController.swift
@@ -10,16 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var devImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.devImage.layer.cornerRadius = 10
+        self.devImage.layer.masksToBounds = true
+     
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
+    
+  
+        
 
 
-}
+
 
